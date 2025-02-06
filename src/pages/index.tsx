31c005f7543b4e8274b1124e4eacd065472bb896
iamwinner422 <iamwinner422@gmail.com>
2025-02-06 22:31:15 +0000

    Adding piece of logic

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 
 import {Input} from "@heroui/input";
+import {Progress} from "@heroui/progress";
 import { title, subtitle } from "@/components/primitives";
 import { LinkIcon } from "@/components/icons";
 import DefaultLayout from "@/layouts/default";
@@ -10,6 +11,8 @@ import { ytLinkRegex } from "@/config/utils";
 export default function IndexPage() {
     const [ytLink, setYtLink] = useState<string>("");
     const [error, setError] = useState<string | undefined>(undefined);
+    const [laoding, setLoading] = useState<boolean>(false);
+
 
     const handleGenerate = () => {
         if(ytLink === ""){
@@ -18,7 +21,8 @@ export default function IndexPage() {
         }else if(ytLinkRegex.test(ytLink) === false){
             setError("Please provide a valid link to the Youtube video.");
             setTimeout(() => setError(undefined), 5000);
-
+        }else{
+            setLoading(true);
         }
     }
     return (
@@ -37,22 +41,28 @@ export default function IndexPage() {
                         <div className="w-full sm:w-[25rem]">
                             <Input type="text" placeholder="Paste your Youtube video link..."
                                 startContent={<LinkIcon />} radius="full" className="w-full" value={ytLink}
-                                onChange={(e) => setYtLink(e.target.value)}
+                                onChange={(e) => setYtLink(e.target.value)} disabled={laoding}
                                 variant="bordered" size="lg" isClearable={true} onClear={() => setYtLink("")}
                             />
                         </div>
-                        <button type="button" onClick={handleGenerate} className="w-full sm:w-auto flex gap-x-2 items-center justify-center px-4 shadow bg-[#b249f8] hover:bg-[#b249f8]/90 font-medium text-white py-3 rounded-full">
+                        <button type="button" disabled={laoding} onClick={handleGenerate} className="w-full sm:w-auto flex gap-x-2 items-center justify-center px-4 shadow bg-[#b249f8] hover:bg-[#b249f8]/90 font-medium text-white py-3 rounded-full">
                             Generate
                             <Sparkles size={18} className="font-semibold"/>
                         </button>
                         
                     </div>
-
-                    <div className="mt-2">
-                        {error && error !== "" &&(
+                    {error && error !== "" &&(
+                        <div className="mt-2">
                             <p className="text-red-500 text-center text-sm">{error}</p>
-                        )} 
-                    </div>
+                        </div>
+                    )} 
+                    
+                    {laoding && (
+                        <div className="mt-16 flex flex-col justify-center items-center">
+                            <p>Processing your video, please wait...</p>
+                            <Progress isIndeterminate aria-label="Loading..." color="secondary" className="mt-2 w-1/2" size="sm" />
+                        </div>
+                    )}
                 </div>
             </section>
         </DefaultLayout>
