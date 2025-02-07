1e9e83cb8e878b326234fa8e31c9dfe1ff78ff9f
iamwinner422 <iamwinner422@gmail.com>
2025-02-07 11:16:00 +0000

    creating skeleton

--- src/components/home/VideoInformations.tsx
+++ src/components/home/VideoInformations.tsx
@@ -1,6 +1,32 @@
 
-export default function VideoInformations() {
+import { Button } from "@heroui/button";
+import { Skeleton } from "@heroui/skeleton";
+import { Card } from "@heroui/Card";
+
+
+interface Props {
+    loading: boolean;
+    error: string | undefined;
+}
+export default function VideoInformations({ loading, error }: Props) {
     return (
-        <div id="video-informations" className={` h-screen w-full`}>VideoInformations</div>
+        <div className="p-8">
+            {loading && (
+                <div className="flex flex-col gap-3">
+                    <Card className="w-full h-full size-full py-8 px-6" radius="lg">
+                    <div className=" w-full flex items-start gap-4">
+                        <div>
+                            <Skeleton className="flex rounded-lg max-w-full w-[400px] h-[250px]" />
+                        </div>
+                        <div className="w-full flex flex-col gap-3">
+                            <Skeleton className="h-4 w-3/5 rounded-lg" />
+                            <Skeleton className="h-4 w-4/5 rounded-lg" />
+                            <Skeleton className="h-4 w-2/5 rounded-lg" />
+                        </div>
+                    </div>
+                    </Card>
+                </div>
+            )}
+        </div>
     )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,113 +10,108 @@ import { useNavigate } from "react-router-dom";
 import VideoInformations from "@/components/home/VideoInformations";
 
 export default function IndexPage() {
-  const [ytLink, setYtLink] = useState<string>("");
-  const [error, setError] = useState<string | undefined>(undefined);
-  const [loading, setLoading] = useState<boolean>(false);
-  const [showVideoInfo, setShowVideoInfo] = useState<boolean>(false);
-  const navigate = useNavigate();
+    const [ytLink, setYtLink] = useState<string>("");
+    const [error, setError] = useState<string | undefined>(undefined);
+    const [loading, setLoading] = useState<boolean>(false);
+    const [showVideoInfo, setShowVideoInfo] = useState<boolean>(false);
+    const navigate = useNavigate();
 
-  // Lorsqu'on affiche les informations vidéo, on effectue le scroll
-  useEffect(() => {
-    if (showVideoInfo) {
-      const element = document.getElementById("video-informations");
-      if (element) {
-        element.scrollIntoView({ behavior: "smooth" });
-      }
-    }
-  }, [showVideoInfo]);
+    // Lorsqu'on affiche les informations vidéo, on effectue le scroll
+    useEffect(() => {
+        if (showVideoInfo) {
+            const element = document.getElementById("video-informations");
+            if (element) {
+                element.scrollIntoView({ behavior: "smooth" });
+            }
+        }
+    }, [showVideoInfo]);
 
-  const handleGenerate = () => {
-    if (ytLink === "") {
-      setError("Please provide a link to the Youtube video.");
-      setTimeout(() => setError(undefined), 5000);
-    } else if (!ytLinkRegex.test(ytLink)) {
-      setError("Please provide a valid link to the Youtube video.");
-      setTimeout(() => setError(undefined), 5000);
-    } else {
-      setLoading(true);
-      setTimeout(() => {
-        setShowVideoInfo(true);
-        setLoading(false);
-        // Optionnel : mise à jour de l'URL avec le hash
-        navigate("/#video-informations", { replace: true });
-      }, 3000);
-    }
-  };
+    const handleGenerate = () => {
+        if (ytLink === "") {
+            setError("Please provide a link to the Youtube video.");
+            setTimeout(() => setError(undefined), 5000);
+        } else if (!ytLinkRegex.test(ytLink)) {
+            setError("Please provide a valid link to the Youtube video.");
+            setTimeout(() => setError(undefined), 5000);
+        } else {
+            setLoading(true);
+            setTimeout(() => {
+                setShowVideoInfo(true);
+                //setLoading(false);
+                navigate("/#video-informations", { replace: true });
+            }, 3000);
+        }
+    };
 
-  return (
-    <DefaultLayout>
-      {/* Section Hero */}
-      <section
-        id="home"
-        className="snap-start flex flex-col items-center justify-center gap-4 py-8 md:pt-16 md:pb-72"
-      >
-        <div className="inline-block max-w-lg text-center">
-          <span className={title({ color: "violet", size: "lg" })}>
-            Auto Clip&nbsp;
-          </span>
-          <br />
-          <div className={subtitle({ class: "mt-4" })}>
-            Generate and download Youtube videos clips in few clicks using AI
-          </div>
-        </div>
+    return (
+        <DefaultLayout>
 
-        <div className="mt-8">
-          <div className="flex flex-wrap gap-x-3 items-center justify-center gap-y-2">
-            <div className="w-full sm:w-[25rem]">
-              <Input
-                type="text"
-                placeholder="Paste your Youtube video link..."
-                startContent={<LinkIcon />}
-                radius="full"
-                className="w-full"
-                value={ytLink}
-                onChange={(e) => setYtLink(e.target.value)}
-                disabled={loading}
-                variant="bordered"
-                size="lg"
-                isClearable={true}
-                onClear={() => setYtLink("")}
-              />
-            </div>
-            <button
-              type="button"
-              disabled={loading}
-              onClick={handleGenerate}
-              className="w-full sm:w-auto flex gap-x-2 items-center justify-center px-4 shadow bg-[#b249f8] hover:bg-[#b249f8]/90 font-medium text-white py-3 rounded-full"
+            <section id="home"
+                className="snap-start flex flex-col items-center justify-center gap-4 py-8 md:pt-16 md:pb-72"
             >
-              Generate
-              <Sparkles size={18} className="font-semibold" />
-            </button>
-          </div>
-          {error && (
-            <div className="mt-2">
-              <p className="text-red-500 text-center text-sm">{error}</p>
-            </div>
-          )}
+                <div className="inline-block max-w-lg text-center">
+                    <span className={title({ color: "violet", size: "lg" })}>
+                        Auto Clip&nbsp;
+                    </span>
+                    <br />
+                    <div className={subtitle({ class: "mt-4" })}>
+                        Generate and download Youtube videos clips in few clicks using AI
+                    </div>
+                </div>
 
-          {loading && (
-            <div className="mt-16 flex flex-col justify-center items-center">
-              <p>Processing your video, please wait...</p>
-              <Progress
-                isIndeterminate
-                aria-label="Loading..."
-                color="secondary"
-                className="mt-2 w-1/2"
-                size="sm"
-              />
-            </div>
-          )}
-        </div>
-      </section>
+                <div className="mt-8">
+                    <div className="flex flex-wrap gap-x-3 items-center justify-center gap-y-2">
+                        <div className="w-full sm:w-[25rem]">
+                            <Input
+                                type="text"
+                                placeholder="Paste your Youtube video link..."
+                                startContent={<LinkIcon />}
+                                radius="full"
+                                className="w-full"
+                                value={ytLink}
+                                onChange={(e) => setYtLink(e.target.value)}
+                                disabled={loading}
+                                variant="bordered"
+                                size="lg"
+                                isClearable={true}
+                                onClear={() => setYtLink("")}
+                            />
+                        </div>
+                        <button
+                            type="button"
+                            disabled={loading}
+                            onClick={handleGenerate}
+                            className="w-full sm:w-auto flex gap-x-2 items-center justify-center px-4 shadow bg-[#b249f8] hover:bg-[#b249f8]/90 font-medium text-white py-3 rounded-full"
+                        >
+                            Generate
+                            <Sparkles size={18} className="font-semibold" />
+                        </button>
+                    </div>
+                    {error && (
+                        <div className="mt-2">
+                            <p className="text-red-500 text-center text-sm">{error}</p>
+                        </div>
+                    )}
 
-      {/* Section Video Informations */}
-      <section
-        id="video-informations"
-        className={`snap-start h-screen w-full ${showVideoInfo ? "" : "hidden"}`}
-      >
-        <VideoInformations />
-      </section>
-    </DefaultLayout>
-  );
+                    {loading && (
+                        <div className="mt-16 flex flex-col justify-center items-center">
+                            <p>Processing your video, please wait...</p>
+                            <Progress
+                                isIndeterminate
+                                aria-label="Loading..."
+                                color="secondary"
+                                className="mt-2 w-1/2"
+                                size="sm"
+                            />
+                        </div>
+                    )}
+                </div>
+            </section>
+
+        
+            <section id="video-informations" className={`snap-start h-screen w-full ${showVideoInfo ? "" : "hidden"}`}>
+                <VideoInformations loading={loading}/>
+            </section>
+        </DefaultLayout>
+    );
 }
