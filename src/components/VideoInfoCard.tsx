1afe55a897863aad47659be3c5c2c603975f5249
iamwinner422 <iamwinner422@gmail.com>
2025-02-10 17:34:49 +0000

    refactor VideoInfoCard layout for improved responsiveness and styling

--- src/components/VideoInfoCard.tsx
+++ src/components/VideoInfoCard.tsx
@@ -13,11 +13,11 @@ interface Props {
 export default function VideoInfoCard({ videoInfo, manualClip }: Props) {
     return (
         <div className=" w-full flex flex-col md:flex-row items-start  gap-4 relative">
-            <Card isFooterBlurred={true} isBlurred={true} className="min-w-full md:min-w-[400px] h-[200px] md:h-[250px]">
+            <Card isFooterBlurred={true} isBlurred={true} className="min-w-full md:min-w-[400px]">
                 <Image isBlurred={true}
                     alt={videoInfo?.title}
                     src={videoInfo?.thumbnail}
-                    height="230px"
+                    className="aspect-auto h-[230px] md:h-[250px]"
                     width="100%"
                 />
                 <CardFooter className="absolute bg-background/30 bottom-0 border-t-1 border-zinc-100/20 z-10 justify-between">
@@ -31,15 +31,17 @@ export default function VideoInfoCard({ videoInfo, manualClip }: Props) {
                     </div>
                 </CardFooter>
             </Card>
-            <div className="w-full flex flex-col gap-3 px-3">
+            <div className="overflow-x-auto w-full flex flex-col gap-3 px-3 ">
                 <p className="uppercase font-bold md:text-2xl text-xl truncate text-wrap">{videoInfo?.title}</p>
                 {manualClip && (
-                    <div className="my-6 flex flex-col mg:flex-row gap-x-2 gap-y-3 md:absolute md:bottom-0">
-                        <div className="flex items-center w-full gap-x-3">
+                    <div className="mt-6 mb-6 md:mb-0 w-full flex items-center justify-start flex-col sm:flex-row gap-x-6 gap-y-3 lg:absolute lg:bottom-0">
+                        <div className="flex items-center gap-x-3">
                             <Input type="number" id="start-time" size="lg" placeholder="Start Time" className="h-12 w-1/2 sm:w-32 rounded-md" />
                             <Input type="number" id="duration" size="lg" placeholder="Duration" className="h-12 w-1/2 sm:w-32 rounded-md" />
                         </div>
-                        <Button className="w-full md:w-40" size="lg" color="primary">Download</Button>
+                        <div className="w-full">
+                            <Button className="w-full " size="lg" color="primary">Download</Button>
+                        </div>
                     </div>
                 )}
             </div>
